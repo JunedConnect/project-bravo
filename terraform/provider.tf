73950d60f4e79f4e0e2c31c04e95a1c174ecf100
JunedConnect <Junedwork@hotmail.com>
2025-05-03 19:46:55 +0100

    provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -49,15 +49,23 @@ provider "aws" {
 
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = module.eks.cluster_name
+  host                   = module.eks.eks-cluster-endpoint
+  cluster_ca_certificate = base64decode(module.eks.eks-cluster-ca-data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.eks-cluster-name]
+    command     = "aws"
+  }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = module.eks.cluster_name
+    host                   = module.eks.eks-cluster-endpoint
+    cluster_ca_certificate = base64decode(module.eks.eks-cluster-ca-data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.eks-cluster-name]
+      command     = "aws"
+    }
   }
 }
