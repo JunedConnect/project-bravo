aedb1a2503283cb002c538ae05f5e787c8f55afe
JunedConnect <Junedwork@hotmail.com>
2025-05-03 21:47:44 +0100

    cicd fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -23,31 +23,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1"
-#     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#     command     = "aws"
-#   }
-# }
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.eks.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1"
-#       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#       command     = "aws"
-#     }
-#   }
-# }
-
-
-
 provider "kubernetes" {
   host                   = module.eks.eks-cluster-endpoint
   cluster_ca_certificate = base64decode(module.eks.eks-cluster-ca-data)
